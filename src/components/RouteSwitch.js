35db9e2428148b8b5c26c85590693380ac6443c7
Seth Feldman <namdlefhtes@gmail.com>
2022-07-19 08:48:37 -0400

    rename cart page component

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import ShoppingCart from "./ShoppingCart";
+import cartPage from "./cartPage";
 
 const RouteSwitch = (props) => {
     return (
---
+++ src/components/cartPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const cartPage = (props) =>{
+ 
+}
+
+export default cartPage;
