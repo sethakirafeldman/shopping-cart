db2092e5ab7765af86ecc871ea5390e8d2329ef9
Seth Feldman <namdlefhtes@gmail.com>
2022-07-22 15:16:31 -0400

    new assets

---
+++ src/data.js
@@ -0,0 +1,51 @@
+import capo from "./assets/capo.jpg";
+import stringwinder from "./assets/stringwinder.jpg";
+import guitarpicks from "./assets/guitarpicks.jpg";
+import guitarstand from "./assets/guitarstand.jpg";
+import acousticcase from "./assets/acousticcase.jpg";
+const data =
+{"products": [
+    {
+        "name": "Guitar Capo",
+        "index":0,
+        "description": "Quickly change the key of the song with this capo.",
+        "price": "8.99",
+        "src": capo
+    },
+    
+    {
+        "name": "String Winder and Cutter",
+        "index": 1,
+        "description": "Resting your guitar in record time with this tool!",
+        "price": "16.99",
+        "src": stringwinder
+    },
+    
+    {
+        "name": "Assorted Picks",
+        "index": 2,
+        "description": "Strum or pick with these quality plectrums",
+        "price": "9.99",
+        "src": guitarpicks
+    },
+    
+    {
+        "name": "Acoustic Gig Bag",
+        "index": 3,
+        "description": "Bring your acoustic on the road",
+        "price": "32.99",
+        "src": acousticcase
+    },
+    
+    {
+        "name": "A-Style Guitar Stand",
+        "index": 4,
+        "description": "Take a stand",
+        "price": "14.99",
+        "src": guitarstand
+    }  
+]   
+
+}
+
+export default data;
