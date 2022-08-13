457d6b990c60c272a4863ea4961ec460cb3db749
Seth Feldman <namdlefhtes@gmail.com>
2022-08-13 11:37:17 -0400

    prodcard test

--- src/ProductCard.test.js
+++ src/ProductCard.test.js
@@ -2,14 +2,25 @@ import React from 'react';
 import { render, screen } from "@testing-library/react";
 import ProductCard from "../src/components/ProductCard";
 import '@testing-library/jest-dom'
-import { BrowserRouter } from "react-router-dom";
-import userEvent from "@testing-library/user-event";
+import { within } from '@testing-library/react';
+import data from "./data";
 
+test('available products render', async ()=> {
+    render( 
+    
+    <ProductCard
+      data = {data}
+    />
 
-test('page switches', async ()=> {
-    //    const user = userEvent.setup();
+    )
+    const item0 = document.getElementById("item-0");
+    const item1 = document.getElementById("item-1");
+    const checkItem0 = within(item0).getByText(/8.99/i);
+    const checkItem1 = within(item1).getByText(/Stay in tune with this clip on./i);
 
-       //verify cart page link
+    expect(checkItem0).toBeInTheDocument();
+    expect(checkItem1).toBeInTheDocument();
+
     //    const cartEl = screen.getByTestId('cart-test');
     //    await user.click(cartEl);
     //    expect(screen.getByText(/cart/i)).toBeInTheDocument();
