f1a672b4c7ecda0440d8a1cdc62062bd88900353
Seth Feldman <namdlefhtes@gmail.com>
2022-08-17 08:13:48 -0400

    transition group for cart

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ProductCard from './ProductCard';
+import CartPage from './CartPage';
+import {CSSTransition} from 'react-transition-group';
 
 const Home = (props) =>{
 
+    console.log(props.cartDrawer)
     return (
-    <>  
-        <ProductCard 
-            data = {props.data}
-            handleKeyPress = {props.handleKeyPress}
-            handleChange = {props.handleChange}
-            addToCart = {props.addToCart}
-            handleIncrement = {props.handleIncrement}
-        />            
+    <>    
+        <h1>Products</h1>
+        <section id="page-content">  
+            <ProductCard 
+                data = {props.data}
+                handleKeyPress = {props.handleKeyPress}
+                handleChange = {props.handleChange}
+                addToCart = {props.addToCart}
+                handleIncrement = {props.handleIncrement}
+            />            
+    
+        
+        {/* {props.cartDrawer ?  */}
+
+        <CSSTransition 
+            in={props.cartDrawer}
+            classNames="cart-drawer"
+            timeout={200} 
+        >
+            {<CartPage 
+                    cartItems = {props.cartItems}
+                    data = {props.data}
+                    cartDrawer = {props.cartDrawer}
+                /> }
+        </CSSTransition>
+            
+        {/* // : 
+        // null } */}
+        </section>
     </>
     )
 }
